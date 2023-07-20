12e0b954a793ad8382d5a70a0369abcfecaae0ca
AdrianDiazAndreu <adriandiazandreu@gmail.com>
2023-07-20 19:27:07 +0200

    tailwind update 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
       return (
 
-            <div className=''>
-                  
+            <div className='text-red-500'>
+                  hola
             </div>
     
       )
